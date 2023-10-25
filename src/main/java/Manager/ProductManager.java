435ee0ae6467e57a822ed959a3527b7f44abbf70
BogdanNC <bogdancristi012@gmail.com>
2023-10-25 12:52:45 +0300

    add products in the mongodb function

--- src/main/java/Manager/ProductManager.java
+++ src/main/java/Manager/ProductManager.java
@@ -1,27 +1,40 @@
 package Manager;
 
-import Database.Database;
+import Database.MongoDB;
 import Products.Product;
 import Products.ProductSeries;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
 
 import java.util.Scanner;
 
 public class ProductManager {
-    Database database = Database.getDatabase();
     Scanner scanner = new Scanner(System.in);
 
     public void getProductsCommand () {
         System.out.println("10.1 - add product, 10.2 - add product series at a certain product type");
         String action = scanner.next();
         if (action.equals("10.1")) {
-            addProduct();
+            try {
+                addProduct();
+            } catch (Exception err) {
+                System.out.println(err.getMessage());
+            }
         } else  if (action.equals("10.2")) {
             addProductSeries();
         } else {
             System.out.println("comanda invalida");
         }
     }
-    public void addProduct () {
+    public void addProduct () throws Exception {
+
+        MongoDB mongoClient = new MongoDB();
+        MongoClient client = mongoClient.connectDB();
+
+        MongoCollection<Document> productCollection = mongoClient.getCollection("products", client);
+
         System.out.println("product generalName: ");
         String name = scanner.next();
 
@@ -31,10 +44,20 @@ public class ProductManager {
         System.out.println("product producer: ");
         String producer = scanner.next();
 
-        Integer productId = assignProductID();
+        System.out.println("assign product ID: ");
+        Integer productId = scanner.nextInt();
+       // Integer productId = assignProductID();
 
-        Product product = new Product(productId, producer, name, category);
+        //Product product = new Product(productId, producer, name, category);
+        if (productCollection.find(Filters.eq("productId", productId)).first() != null) {
+            if (productCollection.find(Filters.eq("producer", producer)).first() != null)) {
+                throw new Exception("Already existing Id by this producer");
+            }
+        }
 
+        Document ProductDocument = new Document("name", name).append("category", category).
+                append("producer", producer).append("productId", productId);
+        productCollection.insertOne(ProductDocument);
         //to do : store product to database
 
     }
