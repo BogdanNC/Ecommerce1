cba4f24aaec5337b5f0b2c619cc221a2f0633312
Bogdan Nicoara <bogdancristi012@gmail.com>
2023-10-28 22:45:44 +0300

    add product and productSeries function added

--- src/main/java/Manager/ProductManager.java
+++ src/main/java/Manager/ProductManager.java
@@ -1,12 +1,15 @@
 package Manager;
 
+import Classes.ProductPrice;
 import Database.MongoDB;
 import Products.Product;
 import Products.ProductSeries;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 import java.util.Scanner;
 
@@ -23,7 +26,11 @@ public class ProductManager {
                 System.out.println(err.getMessage());
             }
         } else  if (action.equals("10.2")) {
-            addProductSeries();
+            try {
+                addProductSeries();
+            } catch (Exception err) {
+                System.out.println(err.getMessage());
+            }
         } else {
             System.out.println("comanda invalida");
         }
@@ -46,11 +53,10 @@ public class ProductManager {
 
         System.out.println("assign product ID: ");
         Integer productId = scanner.nextInt();
-       // Integer productId = assignProductID();
 
-        //Product product = new Product(productId, producer, name, category);
         if (productCollection.find(Filters.eq("productId", productId)).first() != null) {
-            if (productCollection.find(Filters.eq("producer", producer)).first() != null)) {
+            if (productCollection.find(Filters.eq("producer", producer)).first() != null) {
+                client.close();
                 throw new Exception("Already existing Id by this producer");
             }
         }
@@ -58,14 +64,64 @@ public class ProductManager {
         Document ProductDocument = new Document("name", name).append("category", category).
                 append("producer", producer).append("productId", productId);
         productCollection.insertOne(ProductDocument);
-        //to do : store product to database
+        client.close();
 
     }
-    private Integer assignProductID () {
-        //to do : assign unique Product ID;
-        return 0;
-    }
-    public void addProductSeries () {
+    public void addProductSeries () throws Exception {
+        MongoDB mongoClient = new MongoDB();
+        MongoClient client = mongoClient.connectDB();
 
+        MongoCollection<Document> productSeriesCollection = mongoClient.getCollection("productSeries", client);
+        MongoCollection<Document> productCollection = mongoClient.getCollection("products", client);
+
+        System.out.println("what is the product ID: ");
+        Integer productId = scanner.nextInt();
+
+        System.out.println("who is the producer ?");
+        String producer = scanner.next();
+
+        FindIterable<Document> productsWithId = productCollection.find(Filters.eq("productId", productId));
+
+        if (productsWithId.first() != null) {
+            Document product = productsWithId.filter(Filters.eq("producer", producer)).first();
+            if (product != null) {
+                System.out.println(product.toString());
+                ObjectId ParentID = product.getObjectId("_id");
+                String objName = product.getString("name");
+                String category = product.getString("category");
+
+                System.out.println("product name type: ");
+                String finalProductName = scanner.next();
+
+                System.out.println("product price: ");
+                String priceString = scanner.next();
+                ProductPrice productPrice = new ProductPrice(priceString, "lei", "19");
+
+                System.out.println("product size : ");
+                String size = scanner.next();
+
+                System.out.println("product quantity: ");
+                Integer quantity = scanner.nextInt();
+
+                System.out.println("product seriesId : ");
+                String seriesId = scanner.next();
+
+                ProductSeries subProduct = new ProductSeries(productId, producer, objName, category, productPrice, quantity, seriesId, ParentID, finalProductName, size );
+
+                Document subProductDocument = new Document("productId", productId).append("producer", producer).
+                        append("name", objName).append("category", category).
+                        append("price", priceString).append("quantity", quantity).append("size", size).append("seriesId",seriesId).
+                        append("ParentID", ParentID).append("finalProductName",finalProductName);
+                productSeriesCollection.insertOne(subProductDocument);
+                client.close();
+
+            } else {
+                client.close();
+                throw new Exception ("there is no such producer");
+            }
+        } else {
+            client.close();
+            throw new Exception ("there is no such ID");
+        }
     }
 }
--- src/main/java/Products/ProductSeries.java
+++ src/main/java/Products/ProductSeries.java
@@ -1,6 +1,7 @@
 package Products;
 
 import Classes.ProductPrice;
+import org.bson.types.ObjectId;
 
 public class ProductSeries extends Product {
 
@@ -8,7 +9,10 @@ public class ProductSeries extends Product {
     private ProductPrice price;
     private Integer quantity;
     private String seriesId;
+    private ObjectId parentId;
     private String finalProductName;
+
+    private String size;
 
     public ProductSeries(Integer productID, String producer,
                          String generalName, String category) {
@@ -16,22 +20,26 @@ public class ProductSeries extends Product {
     }
 
     public ProductSeries (Product product, ProductPrice price,
-                          Integer quantity, String series, String finalProductName) {
+                          Integer quantity, String series, ObjectId parentId, String finalProductName, String size) {
         super(product);
         this.price = price;
         this.quantity = quantity;
         this.seriesId = series;
+        this.parentId = parentId;
         this.finalProductName = finalProductName;
+        this.size = size;
     }
 
     public ProductSeries(Integer productID, String producer, String generalName,
                          String category, ProductPrice price, Integer quantity,
-                         String series, String finalProductName) {
+                         String series, ObjectId parentId, String finalProductName, String size) {
         super(productID, producer, generalName, category);
         this.price = price;
         this.quantity = quantity;
         this.seriesId = series;
+        this.parentId = parentId;
         this.finalProductName = finalProductName;
+        this.size = size;
     }
 
     public ProductPrice getPrice() {
